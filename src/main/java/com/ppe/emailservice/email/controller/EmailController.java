0bcf584654519287171360d7fca610f293b826e3
rblbabu@gmail.com <rblbabu@gmail.com>
2020-06-20 02:15:48 -0400

    updated the api method

--- src/main/java/com/ppe/emailservice/email/controller/EmailController.java
+++ src/main/java/com/ppe/emailservice/email/controller/EmailController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class EmailController {
 	 * 
 	 */
 	@CrossOrigin(origins = "*", maxAge = 3600)
-	@RequestMapping("/processOrderEventsForEmail")
+	@PostMapping("/processOrderEventsForEmail")
 	public void processOrderEventsForEmail() {
 		System.out.println("called");
 		// Simulate a Consumer reading from MQ
